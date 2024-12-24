573d5eebc3bcef42067480fca7fe606967130c1a
ec <unshackledcultist@gmail.com>
2024-12-24 21:13:49 +0530

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello and welcome to "  << "!\n";
+
+    for (int i = 1; i <= 5; i++) {
+        std::cout << "i = " << i << std::endl;
+    }
+
+    return 0;
+}
