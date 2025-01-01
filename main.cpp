6dedaad199ca55be6b933e016b902d96d8c3a8b2
me <66961093+EvilCultist@users.noreply.github.com>
2025-01-02 00:39:09 +0530

    glfw works, opengl still needs to be installed systemwide tho

--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
-struct point2d {
-    double x;
-    double y;
-};
+int main(void)
+{
+    GLFWwindow* window;
 
-int main() {
-    std::cout << "Hello and welcome to "  << "!\n";
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
 
-    for (int i = 1; i <= 5; i++) {
-        std::cout << "i = " << i << std::endl;
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
     }
 
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
